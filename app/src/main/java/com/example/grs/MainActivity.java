0036591f26e7f5414a6b2a17434330f4793accd2
pranjal Jain <prnjljn@gmail.com>
2019-07-04 19:45:13 +0530

    Made basic layout

--- app/src/main/java/com/example/grs/MainActivity.java
+++ app/src/main/java/com/example/grs/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.grs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText mSearchBoxEditText;
+    TextView mUrlDisplayTextView;
+    TextView  mSearchResultsTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSearchBoxEditText = findViewById(R.id.search_box);
+        mUrlDisplayTextView = findViewById(R.id.url_view);
+        mSearchResultsTextView = findViewById(R.id.results_view);
     }
 }
