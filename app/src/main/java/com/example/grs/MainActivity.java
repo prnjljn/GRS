a241cabd57a8230444a1cea9ccf09596d43b1de8
pranjal Jain <prnjljn@gmail.com>
2019-07-07 21:47:24 +0530

    menu added

--- app/src/main/java/com/example/grs/MainActivity.java
+++ app/src/main/java/com/example/grs/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.grs;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText mSearchBoxEditText;
@@ -17,4 +21,22 @@ public class MainActivity extends AppCompatActivity {
         mUrlDisplayTextView = findViewById(R.id.url_view);
         mSearchResultsTextView = findViewById(R.id.results_view);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public  boolean  onOptionsItemSelected(MenuItem item){
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search) {
+            Context context = MainActivity.this;
+            String textToShow = "Search clicked";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
